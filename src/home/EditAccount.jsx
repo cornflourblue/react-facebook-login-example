6ff8cf3e24c02c24c728506aff53e5176f1e90c4
Jason Watmore <cornflourblue@hotmail.com>
2020-10-24 19:05:17 +1100

    tweaked edit account component

--- src/home/EditAccount.jsx
+++ src/home/EditAccount.jsx
@@ -11,12 +11,12 @@ function EditAccount({ history, match }) {
     const { register, handleSubmit, setValue, formState } = useForm();
 
     // pre-populate form with account details when component loads
-    const [account, setAccount] = useState({});
+    const [account, setAccount] = useState(null);
     useEffect(() => {
         accountService.getById(id).then(account => {
+            setAccount(account);
             const fields = ['name', 'extraInfo'];
             fields.forEach(field => setValue(field, account[field]));
-            setAccount(account);
         });
     }, [id, setValue]);
 
@@ -32,35 +32,39 @@ function EditAccount({ history, match }) {
         <div>
             <h2>Edit Account</h2>
             <p>Updating the information here will only change it inside this application, it won't (and can't) change anything in the associated Facebook account.</p>
-            <form onSubmit={handleSubmit(onSubmit)} style={!account.id ? { display: 'none' } : {}}>
-                <div className="form-group">
-                    <label>Facebook Id</label>
-                    <div>{account.facebookId}</div>
-                </div>
-                <div className="form-group">
-                    <label>Name</label>
-                    <input name="name" type="text" ref={register} className="form-control" />
-                </div>
-                <div className="form-group">
-                    <label>Extra Info</label>
-                    <input name="extraInfo" type="text" ref={register} className="form-control" />
-                </div>
-                <div className="form-group">
-                    <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary">
-                        {formState.isSubmitting &&
-                            <span className="spinner-border spinner-border-sm mr-1"></span>
+            {account &&
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-group">
+                        <label>Facebook Id</label>
+                        <div>{account.facebookId}</div>
+                    </div>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input name="name" type="text" ref={register} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Extra Info</label>
+                        <input name="extraInfo" type="text" ref={register} className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary">
+                            {formState.isSubmitting &&
+                                <span className="spinner-border spinner-border-sm mr-1"></span>
+                            }
+                            Save
+                        </button>
+                        <Link to=".." className="btn btn-link">Cancel</Link>
+                        {error &&
+                            <div className="alert alert-danger mt-3 mb-0">{error}</div>
                         }
-                        Save
-                    </button>
-                    <Link to=".." className="btn btn-link">Cancel</Link>
-                    {error &&
-                        <div className="alert alert-danger mt-3 mb-0">{error}</div>
-                    }
+                    </div>
+                </form>
+            }
+            {!account &&
+                <div className="text-center p-3">
+                    <span className="spinner-border spinner-border-lg align-center"></span>
                 </div>
-            </form>
-            <div className="text-center p-3" style={account.id ? { display: 'none' } : {}}>
-                <span className="spinner-border spinner-border-lg align-center"></span>
-            </div>
+            }
         </div>
     );
 }
